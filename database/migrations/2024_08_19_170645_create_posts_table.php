aa976580554571421266f08aaa3fdc850ec4a44d
Juan Pablo Tabares Rico <tabaresricojuanpablo@utp.edu.co>
2024-08-19 11:20:00 -0600

    created post factory and seeder

--- database/migrations/2024_08_19_170645_create_posts_table.php
+++ database/migrations/2024_08_19_170645_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('author');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
